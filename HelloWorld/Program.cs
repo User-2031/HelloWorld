599b3f65a885ebb010dfd5eb51d2158623c3b555
User-2031 <pras3264@gmail.com>
2022-09-27 15:45:51 +0530

    Added new line

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,6 +14,7 @@ namespace HelloWorld
             Console.WriteLine("Today is Tuesday");
             Emp emp = new Emp { Id = 20, Name = "Tom", Salary = 40000 };
             Console.WriteLine($"{emp.Id} {emp.Name} {emp.Salary}");
+            Console.WriteLine("Tomorrow is Wednesday");
         }
     }
 }
